ff7f91fdd55c1ab5de698731e7f478a51fe6e606
mwholey <mwholey@expediagroup.com>
2021-03-18 19:41:24 -0500

    More functionalization and tests, simplifying some of the more complex operations

--- DLinkedList_tests.py
+++ DLinkedList_tests.py
@@ -15,7 +15,7 @@ class TestLinkedList(unittest.TestCase):
         self.assertEqual(dl.tail.value, 1)
 
         dl.add_node(Node(2))
-        self.assertEqual(list(dl), [0, 1, 2])
+        self.assertEqual(dl.value_list(), [0, 1, 2])
 
     def test_annihilate(self):
         dl = DLinkedList(Node(0))
@@ -25,7 +25,25 @@ class TestLinkedList(unittest.TestCase):
         e4 = dl.head.next.next
         dl.annihilate_with_next(e4)
 
-        self.assertEqual(list(dl), [0, 1, 4])
+        self.assertEqual(dl.value_list(), [0, 1, 4])
+
+    def test_remove_all_of_value(self):
+        dl = DLinkedList()
+        for i in list(range(-9, 10))*2:
+            dl.add_node(Node(i))
+
+        dl.remove_all_of_value(5)
+        self.assertFalse(5 in dl.value_list())
+
+        for i in [9, 8, 10, -3, 0]:
+            dl.remove_all_of_value(i)
+        self.assertFalse(8 in dl.value_list() or 9 in dl.value_list())
+
+        for i in [1, 2, 4]:
+            dl.remove_all_of_value(i)
+        
+        self.assertEqual(dl.value_list(), [-7, -6, 6, 7, -7, -6, 6, 7])
+        
 
 
 if __name__ == '__main__':
--- collapsing_ropes.py
+++ collapsing_ropes.py
@@ -45,29 +45,63 @@ class DLinkedList:
     def __iter__(self):
         node = self.head
         while node is not None:
-            yield node.value
+            yield node
             node = node.next
+
+    def value_list(self):
+        l = []
+        for node in list(self):
+            l.append(node.value)
+        return list(l)
+
+    def value_sum(self):
+        sum(self.value_list())
+    
+    def set_head(self, node):
+        node.prev = None
+        self.head = node
+
+    def set_tail(self, node):
+        node.next = None
+        self.tail = node
 
     def link_node(self, before, after):
         before.next = after
         after.prev = before
 
     def add_node(self, node):
-        if self.tail != self.head:
-            self.link_node(self.tail, node)
+        if self.head is None and self.tail is None:
+            self.head = node
         else:
-            self.link_node(self.head, node)
+            if self.tail != self.head:
+                self.link_node(self.tail, node)
+            else:
+                self.link_node(self.head, node)
 
         self.tail = node
 
     def annihilate_with_next(self, node):
         if node.next:
+            if not node.next.next:
+                self.set_tail(node.prev)
             if node.prev:
                 self.link_node(node.prev, node.next.next)
             else:
-                self.head = node.next.next
+                self.set_head(node.next.next)
         else:
             raise AnnihilationError('Tried to annihilate end of list')
+
+    def remove_all_of_value(self, value):
+        for node in list(self):
+            if abs(node.value) == abs(value):
+                if node.prev is not None:
+                    if node.next is not None:
+                        self.link_node(node.prev, node.next)
+                    else:
+                        self.set_tail(node.prev)
+                else:
+                    self.set_head(node.next)
+
 
 
 
